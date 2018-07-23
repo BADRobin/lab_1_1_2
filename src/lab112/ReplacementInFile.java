018abcc0acb7afdbd72a8ad89ba7eecf40883aef
BADRobin <olegbryl@gmail.com>
2018-07-23 21:36:34 +0300

    lab112_3

---
+++ src/lab112/ReplacementInFile.java
@@ -0,0 +1,19 @@
+package lab112;
+
+import java.io.*;
+
+public class ReplacementInFile {
+    public static void main(String[] args){
+        try (BufferedReader reader = new BufferedReader(new FileReader("User.java"));
+        PrintWriter writer = new PrintWriter("User.java")) {
+            reader.lines().map(s -> s.replace("public", "private"));
+//            String line;
+//            while ((line = reader.readLine())!=null){
+//                writer.println(line.replace("public", "privet"));
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/lab112/User.java
@@ -0,0 +1,47 @@
+package lab112;
+
+public class User {
+    public User(int id, int name, int lastName, double weight) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.weight = weight;
+    }
+
+    private int id;
+    private int name;
+    private int lastName;
+    private double weight;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
